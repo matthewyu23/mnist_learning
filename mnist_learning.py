70dabe98e07c3aecd8e921c49e206d0c3bbabc49
matthewyu23 <matthewyu23@berkeley.edu>
2021-05-22 15:19:36 +0800

    update

--- mnist_learning.py
+++ mnist_learning.py
@@ -2,31 +2,16 @@ from keras.datasets import mnist
 import numpy as np
 
 (train_X, train_y), (test_X, test_y) = mnist.load_data()
-train_X = np.array(train_X) / 255
-train_y = np.array(train_y)
-test_X = np.array(test_X) / 255
-test_y = np.array(test_y)
+train_X = np.array(train_X)[:1000] / 255
+train_y = np.array(train_y)[:1000]
+test_X = np.array(test_X)[:1000] / 255
+test_y = np.array(test_y)[:1000]
 
 np.random.seed(1)
 
 
-def relu(x):
-    answer = []
-    for i in x[0]: 
-        if i > 0: 
-            answer.append(i)
-        else: 
-            answer.append(0)
-    return np.array([answer])
-
-def relu2(x): 
-    answer = []
-    for i in x[0]: 
-        if i > 0: 
-            answer.append(1)
-        else: 
-            answer.append(0)
-    return np.array([answer])
+relu = lambda x:(x>=0) * x
+relu2 = lambda x: x>=0
 
 def max_elem(x): 
     arr = x[0]
@@ -43,44 +28,41 @@ def max_elem(x):
 
 np.random.seed(1)
 
-alpha = 0.0001
-hidden_layer_size = 64
-weights_0_1 = 2*np.random.random((784, 128)) - 1 
-weights_1_2 = 2*np.random.random((128, 64)) - 1 
-weights_2_3 = 2*np.random.random((64, 10)) - 1 
+alpha = 0.005
+weights_0_1 = 0.1 * (2*np.random.random((784, 40)) - 1)
+weights_1_2 = 0.1 * (2*np.random.random((40, 10)) - 1) 
 
 
 
-for iteration in range(6): 
-    layer_3_error = 0
+for iteration in range(350): 
+    layer_2_error = 0
     for i in range(len(train_X)): 
         layer_0 = train_X[i]
         layer_0 = np.array([layer_0.flatten()])
         layer_1 = relu(np.dot(layer_0, weights_0_1))
-        layer_2 = relu(np.dot(layer_1, weights_1_2))
-        layer_3 = np.dot(layer_2, weights_2_3)
+        layer_2 = np.dot(layer_1, weights_1_2)
         
         
 
         answer = np.zeros(10)
         answer[train_y[i]] = 1
-        layer_3_error += np.sum((layer_3 - answer) ** 2)
-        
+        answer = [answer]
+        layer_2_error += np.sum((answer - layer_2) ** 2)
+   
 
         
-        layer_3_delta = (layer_3 - answer) 
-        layer_2_delta = np.dot(layer_3_delta, weights_2_3.T) * relu2(layer_2)
+        layer_2_delta = np.array(answer - layer_2)
         layer_1_delta = np.dot(layer_2_delta, weights_1_2.T) * relu2(layer_1)
 
-        weights_2_3 -= alpha * layer_2.T.dot(layer_3_delta)
+
         weights_1_2 -= alpha * layer_1.T.dot(layer_2_delta)
         weights_0_1 -= alpha * layer_0.T.dot(layer_1_delta)
 
 
 
+    
 
-        
-    print(layer_3_error)
+    print(layer_2_error)
 
 
 score = 0 
@@ -88,10 +70,10 @@ for i in range(len(test_X)):
     layer_0 = np.array([test_X[i].flatten()])
     layer_1 = relu(np.dot(layer_0, weights_0_1))
     layer_2 = relu(np.dot(layer_1, weights_1_2))
-    layer_3 = np.dot(layer_2, weights_2_3)
+    
 
 
-    prediction = max_elem(layer_3)
+    prediction = max_elem(layer_2)
     answer = test_y[i]
     
 
